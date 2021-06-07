85eb9a53cfcf0500b4ee32442850369c6d8aa4f3
Jeffrey North <JNorth@tql.com>
2021-06-07 12:57:48 -0400

    Added controllers and DB context

---
+++ PrsCapstoneBackEnd/Data/PrsCapstoneBackEndContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrsCapstoneBackEnd.Models;
+
+namespace PrsCapstoneBackEnd.Data
+{
+    public class PrsCapstoneBackEndContext : DbContext
+    {
+        public PrsCapstoneBackEndContext (DbContextOptions<PrsCapstoneBackEndContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PrsCapstoneBackEnd.Models.Product> Product { get; set; }
+
+        public DbSet<PrsCapstoneBackEnd.Models.Request> Request { get; set; }
+
+        public DbSet<PrsCapstoneBackEnd.Models.RequestLine> RequestLine { get; set; }
+
+        public DbSet<PrsCapstoneBackEnd.Models.User> User { get; set; }
+
+        public DbSet<PrsCapstoneBackEnd.Models.Vendor> Vendor { get; set; }
+    }
+}
--- PrsCapstoneBackEnd/Startup.cs
+++ PrsCapstoneBackEnd/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrsCapstoneBackEnd.Data;
 
 namespace PrsCapstoneBackEnd
 {
@@ -25,6 +27,9 @@ namespace PrsCapstoneBackEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<PrsCapstoneBackEndContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PrsCapstoneBackEndContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
